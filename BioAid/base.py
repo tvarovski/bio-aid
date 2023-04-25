c5e1f44de31bd2d231248af9c673e668cca94c6d
tvarovski <tvarovski1@gmail.com>
2023-04-25 16:00:43 -0500

    add genomic context analysis functions

--- BioAid/base.py
+++ BioAid/base.py
@@ -10,83 +10,189 @@
 ## createChrList
 ## dataFrameImport
 
+import logging
+
 def extractSeqFromFastaToList(fasta_file_path):
-  '''This function takes a path of a fastafile and extracts all sequence names and
-  sequences into a nested list [[title_0, sequence_0], [title_1, sequence_1],...]'''
+    '''This function takes a path of a fastafile and extracts all sequence names and
+    sequences into a nested list [[title_0, sequence_0], [title_1, sequence_1],...]'''
 
-  fasta_file = open(fasta_file_path, 'r')
-  contents = fasta_file.readlines()
-  fasta_file.close()
+    fasta_file = open(fasta_file_path, 'r')
+    contents = fasta_file.readlines()
+    fasta_file.close()
 
-  fasta_list=[]
+    fasta_list=[]
 
-  for i in contents:
-    if '>' in i:
-      fasta_list.append([i.strip('>').strip(),''])
-    else:
-      fasta_list[-1][1] = fasta_list[-1][1]+i.strip()
-  print(f"Extraction of sequence information from {fasta_file_path} finished.")
-  return fasta_list
+    for i in contents:
+        if '>' in i:
+            fasta_list.append([i.strip('>').strip(),''])
+        else:
+            fasta_list[-1][1] = fasta_list[-1][1]+i.strip()
+    print(f"Extraction of sequence information from {fasta_file_path} finished.")
+    return fasta_list
 
 def validateSquence(sequence):
-  '''Check if the sequence contains viable nucleotides only'''
+    '''Check if the sequence contains viable nucleotides only'''
 
-  bases = "ATGCatgc"
-  for i in sequence:
-    if i not in bases:
-      print("warning, sequence doesn't contain cannonical nucleotides")
-      return(False)
-  return(True)
+    bases = "ATGCatgcN"
+    for i in sequence:
+        if i not in bases:
+            print(f"error, sequence doesn't contain cannonical nucleotides: {i}")
+            return(False)
+        if i == "N":
+            print(f"warning, sequence contains 'N's")
+    return(True)
 
 def compl(base):
-  if base == "A":
-    return('T')
-  elif base == "T":
-    return('A')
-  elif base == "G":
-    return('C')
-  elif base == "C":
-    return('G')
-  elif base == "-":
-    return('-')
+    if base == "A":
+        return('T')
+    elif base == "T":
+        return('A')
+    elif base == "G":
+        return('C')
+    elif base == "C":
+        return('G')
+    elif base == "-":
+        return('-')
 
 def rev_compl(seq):
-  new_seq = ""
-  for base in seq:
-    new_base = compl(base)
-    new_seq = new_base + new_seq
-  return(new_seq)
+    new_seq = ""
+    for base in seq:
+        new_base = compl(base)
+        new_seq = new_base + new_seq
+    return(new_seq)
 
 def unique(list1):
-  # this function takes a list1 and returns a list2 with
-  # unique elements of list 
+    # this function takes a list1 and returns a list2 with
+    # unique elements of list 
 
-  list_set = set(list1) 
-  unique_list = (list(list_set))
-  return unique_list
+    list_set = set(list1) 
+    unique_list = (list(list_set))
+    return unique_list
 
 def createChrList(chrNum):
-  chrList = []
-  for i in range(chrNum):
-    chrList.append(f"chr{i+1}")
-  return(chrList)
+    chrList = []
+    for i in range(chrNum):
+        chrList.append(f"chr{i+1}")
+    return(chrList)
 
 def dataFrameImport(directory):
-  import os
-  import pandas as pd
-  frames_list_samples = []
-  sample_names = []
+    import os
+    import pandas as pd
+    frames_list_samples = []
+    sample_names = []
 
-  counter=1
-  for filename in os.listdir(directory):
-      if filename.endswith(".tsv"):
-          sample_names.append(filename[:10].strip())
-          path = os.path.join(directory, filename)
-          globals()[f"sample{counter}"] = pd.read_csv(path, sep='\t')
-          print(f'dataframe {sample_names[-1]} has {len(globals()[f"sample{counter}"])} total rows')
-          frames_list_samples.append(globals()[f"sample{counter}"])
-          counter+=1
-  print(f"found {len(frames_list_samples)} samples in {directory}")
-  print(len(sample_names), sample_names)
+    counter=1
+    for filename in os.listdir(directory):
+            if filename.endswith(".tsv"):
+                    sample_names.append(filename[:10].strip())
+                    path = os.path.join(directory, filename)
+                    globals()[f"sample{counter}"] = pd.read_csv(path, sep='\t')
+                    print(f'dataframe {sample_names[-1]} has {len(globals()[f"sample{counter}"])} total rows')
+                    frames_list_samples.append(globals()[f"sample{counter}"])
+                    counter+=1
+    print(f"found {len(frames_list_samples)} samples in {directory}")
+    print(len(sample_names), sample_names)
 
-  return(frames_list_samples, sample_names)
+    return(frames_list_samples, sample_names)
+
+def pullGenomicContext(list_of_positions, fasta_file_path, context_flank=5):
+    '''This function takes a list of positions [chromosome, position] and a fasta file path and returns a list of
+    genomic context of the specified length around each position'''
+
+    fasta_list = extractSeqFromFastaToList(fasta_file_path)
+    context_list = []
+
+    for i in fasta_list:
+        print(f"extracting context from {i[0]}. It has {len(i[1])} bases.")
+        sequence = i[1]
+        for j in list_of_positions:
+
+            #check if it is the same chromosome
+            if j[0] != i[0]:
+                continue
+            #set j to be the position
+            j = j[1]
+            #find the position in the fasta list
+            #remember that the fasta sequences start with 1, not 0, so we need to subtract 1 from the position
+
+            try:
+                query_base = sequence[j-1]
+
+            except:
+                logging.warning(f"position {j} not found in {i[0]}. Skipping...")
+                continue
+
+            #extract the context
+            context_left = sequence[j-1-context_flank:j-1]
+            context_right = sequence[j:j+context_flank]
+
+            context_list.append([context_left, query_base, context_right, i[0]])
+
+    return(context_list)
+
+def drawGenomicContext(context_list, show=False, **kwargs):
+    '''This function takes a list of genomic contexts and draws a sequence logo of the context.
+    If you pass show=True, it will show the plot.
+    If you pass save_path=<PATH>, it will save the plot to the path.
+    It requires the packages matplotlib.pyplot and panda.s'''
+    import matplotlib.pyplot as plt
+    import pandas as pd
+
+    #create a dataframe from the context list
+    df = pd.DataFrame(context_list, columns=['context_left', '0', 'context_right', 'chromosome'])
+
+    #split the context into columns by base
+    split_left = df['context_left'].apply(lambda x: pd.Series(list(x)))
+    split_right = df['context_right'].apply(lambda x: pd.Series(list(x)))
+
+    #rename the columns such that they are numbered. e.g. -3,-2,-1, 0, 1, 2, 3
+    split_left.columns = [f"-{i}" for i in range(len(split_left.columns), 0, -1)]
+    split_right.columns = [f"{i+1}" for i in range(len(split_right.columns))]
+
+    #make a new dataframe with the context split into columns, query still in the middle
+    df_context = pd.concat([split_left, df['0'], split_right], axis=1)
+
+    df_context_freq = df_context.apply(pd.value_counts).fillna(0)              #get the frequencies of each base in each column
+    df_context_freq = df_context_freq.div(df_context_freq.sum(axis=0), axis=1) #normalize the frequencies
+    df_context_freq = df_context_freq.transpose()
+    df_context_freq = df_context_freq*100                                      #set the frequencies to %
+    
+    #set color scheme for the bases so that they are consistent
+    colors = {
+                'A':'tab:green', 
+                'T':'tab:red',
+                'G':'tab:orange',
+                'C':'tab:blue',
+                'N':'tab:gray',
+                }
+    
+    #plot the dataframe as a stacked barplot
+    column_count = len(df_context_freq.columns) #count the number of columns
+    df_context_freq.plot.bar(stacked=True, figsize=(column_count+3,4), color=colors.values())
+    plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+    plt.ylabel('Frequency (%)')
+    plt.xlabel('Relative Position')
+
+    if 'save_path' in kwargs:
+        plt.savefig(kwargs['save_path'], bbox_inches='tight', dpi=300)
+
+    if show == True:
+        plt.show()
+
+    plt.close()
+
+import pandas as pd
+#open a csv file to a dataframe
+df = pd.read_csv('test_calls.csv')
+
+#extract the positions from the dataframe
+df = df[df["Type"] == "SNV"]
+df = df[df["Reference"] == 'G']
+
+#extract the positions (as integers) from the dataframe
+df['Region'] = df['Region'].astype(int)
+positions = df[['Chromosome','Region']].values.tolist()
+
+context_list = pullGenomicContext(positions, 'S288C_reference_sequencewoUra3wUra329wNAT.fa', context_flank=4)
+
+drawGenomicContext(context_list, save_path='test.png', show=True)
