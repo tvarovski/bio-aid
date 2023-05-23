1d63137af49b0e3a82641a20ecc7e0f0a34ce67d
tvarovski <tvarovski1@gmail.com>
2023-05-23 15:22:22 -0500

    typehints+docstrings

--- BioAid/popDub.py
+++ BioAid/popDub.py
@@ -1,7 +1,18 @@
 # This code was developed and authored by Jerzy Twarowski in Malkova Lab at the University of Iowa 
 # Contact: jerzymateusz-twarowski@uiowa.edu, tvarovski1@gmail.com
 
-def calculatePopulationDoublings(initial_population, final_population_ml, final_culture_volume):
+def calculatePopulationDoublings(initial_population: int|float, final_population_ml: int|float, final_culture_volume: int|float) -> None:
+    """
+    Calculates the number of population doublings required to reach a final population size.
+
+    Args:
+        initial_population (int|float): The initial population size.
+        final_population_ml (int|float): The final population size in milliliters.
+        final_culture_volume (int|float): The final culture volume in milliliters.
+
+    Returns:
+        None: This function does not return anything, but prints the number of population doublings required to reach the final population size.
+    """
     final_population = final_population_ml * final_culture_volume
     doubling_cycle_size = final_population
     doublings = 0
