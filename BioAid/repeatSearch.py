b8ada9480daae056ae55674ceb4b90025c7e62cf
tvarovski <tvarovski1@gmail.com>
2023-05-23 15:49:26 -0500

    typehints+docstrings

--- BioAid/repeatSearch.py
+++ BioAid/repeatSearch.py
@@ -9,14 +9,21 @@
 ## saveToJSON
 
 import regex as re
-import json
 
 #from .base import validateSquence
 from .base import rev_compl
 
 
-def validateDNASquence(sequence):
-    '''Check if the sequence contains viable nucleotides only'''
+def validateDNASquence(sequence: str) -> bool:
+    '''
+    Check if the given DNA sequence contains only valid nucleotides.
+
+    Args:
+        sequence (str): the DNA sequence to validate.
+
+    Returns:
+        bool: True if the sequence contains only valid nucleotides, False otherwise.
+    '''
 
     bases = "ATGCatgc-"
     for i in sequence:
@@ -25,8 +32,21 @@ def validateDNASquence(sequence):
             return(False)
     return(True)
 
-def imperfectHomologySearch(sequence, query, min_homology=0.8, fixed_errors=False, inverted=True):
+def imperfectHomologySearch(sequence: str, query: str, min_homology: float = 0.8, fixed_errors: bool = False, inverted:bool = True) -> list:
+    '''
+    Search for a query sequence in a given DNA sequence, allowing for a certain number of mismatches/imperfect homology.
 
+    Args:
+        sequence (str): the DNA sequence to search in.
+        query (str): the query sequence to search for.
+        min_homology (float, optional): the minimum homology (similarity) between the query and the found sequence, as a fraction between 0 and 1. Defaults to 0.8.
+        fixed_errors (bool, optional): if True, use a fixed number of errors instead of calculating it from min_homology. Defaults to False.
+        inverted (bool, optional): if True, search for the reverse complement of the query sequence as well. Defaults to True.
+
+    Returns:
+        list: a list of query-match pairs, where each pair is a list containing the query sequence and a list of matching sequences.
+    '''
+    errors = 0
     if min_homology:
         errors = round(len(query)*(1-min_homology))
     if fixed_errors:
@@ -47,19 +67,28 @@ def imperfectHomologySearch(sequence, query, min_homology=0.8, fixed_errors=Fals
     else:
         return([])
 
-def findInvertedRepeat(sequence, 
-                                             query_length=4,
-                                             min_spacer=4,
-                                             imperfect_homology=False,
-                                             min_homology=0.8,
-                                             fixed_errors=False,
-                                             inverted=True):
+def findInvertedRepeat(sequence: str, query_length: int = 4, min_spacer: int = 4, imperfect_homology: bool = False,
+                        min_homology: float = 0.8, fixed_errors: bool = False, inverted: bool = True) -> list:
+    '''
+    Search for inverted repeats in a given DNA sequence.
 
+    Args:
+        sequence (str): the DNA sequence to search in.
+        query_length (int, optional): the length of the query sequence to search for. Defaults to 4.
+        min_spacer (int, optional): the minimum number of non-matching nucleotides between the two halves of the inverted repeat. Defaults to 4.
+        imperfect_homology (bool, optional): if True, allow for a certain number of errors in the inverted repeat. Defaults to False.
+        min_homology (float, optional): the minimum homology (similarity) between the query and the found sequence, as a fraction between 0 and 1. Only used if imperfect_homology is True. Defaults to 0.8.
+        fixed_errors (bool, optional): if True, use a fixed number of errors instead of calculating it from min_homology. Only used if imperfect_homology is True. Defaults to False.
+        inverted (bool, optional): if True, search for the reverse complement of the query sequence as well. Defaults to True.
+
+    Returns:
+        list: a list of query-match pairs, where each pair is a list containing the query sequence and a list of matching sequences.
+    '''
     query_string=sequence[:query_length]
 
     if inverted == True:
         query = rev_compl(query_string)
-    if inverted == False:
+    elif inverted == False:
         query = query_string
         
     sequence=sequence[query_length+min_spacer:]
@@ -72,10 +101,10 @@ def findInvertedRepeat(sequence,
             
             
             output_pair=imperfectHomologySearch(sequence[i:query_length+i],
-                                                                                    query,
-                                                                                    min_homology=min_homology,
-                                                                                    fixed_errors=fixed_errors,
-                                                                                    inverted=inverted)
+                                                query,
+                                                min_homology=min_homology,
+                                                fixed_errors=fixed_errors,
+                                                inverted=inverted)
 
             if output_pair != []:
                 output_pair_list.append(output_pair)
@@ -93,16 +122,26 @@ def findInvertedRepeat(sequence,
 
     return(output_pair_list)
         
-def searchSequenceForRepeats(sequence,
-                                                         min_query_length=4,
-                                                         max_query_length=25,
-                                                         min_spacer=0,
-                                                         window_size=250,
-                                                         imperfect_homology=False,
-                                                         min_homology=0.8,
-                                                         fixed_errors=False,
-                                                         inverted=True):
-    
+def searchSequenceForRepeats(sequence: str, min_query_length: int = 4, max_query_length: int = 25,
+                            min_spacer: int = 0, window_size: int = 250, imperfect_homology:bool = False,
+                            min_homology:float = 0.8, fixed_errors:bool = False, inverted: bool = True) -> dict:
+    '''
+    Search a DNA sequence for inverted repeats of various lengths.
+
+    Args:
+        sequence (str): the DNA sequence to search in.
+        min_query_length (int, optional): the minimum length of the query sequence to search for. Defaults to 4.
+        max_query_length (int, optional): the maximum length of the query sequence to search for. Defaults to 25.
+        min_spacer (int, optional): the minimum number of non-matching nucleotides between the two halves of the inverted repeat. Defaults to 0.
+        window_size (int, optional): the size of the sliding window to use when searching for inverted repeats. Defaults to 250.
+        imperfect_homology (bool, optional): if True, allow for a certain number of errors in the inverted repeat. Defaults to False.
+        min_homology (float, optional): the minimum homology (similarity) between the query and the found sequence, as a fraction between 0 and 1. Only used if imperfect_homology is True. Defaults to 0.8.
+        fixed_errors (bool, optional): if True, use a fixed number of errors instead of calculating it from min_homology. Only used if imperfect_homology is True. Defaults to False.
+        inverted (bool, optional): if True, search for the reverse complement of the query sequence as well. Defaults to True.
+
+    Returns:
+        dict: a dictionary where the keys are the query sequences and the values are lists of matching sequences.
+    '''
     if imperfect_homology:
         print(f"Search has been set to find quasi-pallindromes")
         if fixed_errors:
@@ -120,13 +159,13 @@ def searchSequenceForRepeats(sequence,
         
         for i in range(sequence_size): #-window_size+1
             seq=sequence[i:i+window_size]
-            output_pair_list = findInvertedRepeat(seq,
-                                                                                        query_length=query_length,
-                                                                                        min_spacer=min_spacer,
-                                                                                        imperfect_homology=imperfect_homology,
-                                                                                        min_homology=min_homology,
-                                                                                        fixed_errors=fixed_errors,
-                                                                                        inverted=inverted)
+            output_pair_list = findInvertedRepeat(  seq,
+                                                    query_length=query_length,
+                                                    min_spacer=min_spacer,
+                                                    imperfect_homology=imperfect_homology,
+                                                    min_homology=min_homology,
+                                                    fixed_errors=fixed_errors,
+                                                    inverted=inverted)
             for pair in output_pair_list:
                 list_of_all_pairs.append(pair)
 
@@ -142,8 +181,18 @@ def searchSequenceForRepeats(sequence,
     print("Results were consolidated...")
     return(results_dictionary)
 
-def saveToJSON(results_dictionary, output_file_name='json_output.json'):
+def saveToJSON(results_dictionary: dict, output_file_name: str = 'json_output.json') -> None:
+    '''
+    Save a dictionary to a JSON file.
 
+    Args:
+        results_dictionary (dict): the dictionary to save.
+        output_file_name (str, optional): the name of the output file. Defaults to 'json_output.json'.
+
+    Returns:
+        None
+    '''
+    import json
     with open(output_file_name, 'w') as outfile:
             json.dump(results_dictionary, outfile)
 
